d1f6ef557c729e386d3e6546627a13a2ae3d48af
bhartik021 <bhartik021@gmail.com>
2022-07-02 01:32:33 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const App = () => {
   const [result, setResult] = useState("");
 
   const handleClick = (e) => {
-    setResult(result.concat(e.target.value.name));
+    setResult(result.concat(e.target.value));
   }
 
   const clear = () => {
@@ -34,21 +34,21 @@ const App = () => {
         <div className="keypad">
           <button className="highlight" onClick={clear} id="clear">Clear</button>
           <button className="highlight" onClick={backspace} id="backspace">C</button>
-          <button className="highlight" name="/" onClick={handleClick}>&divide;</button>
-          <button name="7" onClick={handleClick}>7</button>
-          <button name="8" onClick={handleClick}>8</button>
-          <button name="9" onClick={handleClick}>9</button>
-          <button className="highlight" name="*" onClick={handleClick}>&times;</button>
-          <button name="4" onClick={handleClick}>4</button>
-          <button name="5" onClick={handleClick}>5</button>
-          <button name="6" onClick={handleClick}>6</button>
-          <button className="highlight" name="-" onClick={handleClick}>&ndash;</button>
-          <button name="1" onClick={handleClick}>1</button>
-          <button name="2" onClick={handleClick}>2</button>
-          <button name="3" onClick={handleClick}>3</button>
-          <button className="highlight" name="+" onClick={handleClick}>+</button>
-          <button name="0" onClick={handleClick}>0</button>
-          <button name="*" onClick={handleClick}>.</button>
+          <button className="highlight" value="/" onClick={handleClick}>&divide;</button>
+          <button value="7" onClick={handleClick}>7</button>
+          <button value="8" onClick={handleClick}>8</button>
+          <button value="9" onClick={handleClick}>9</button>
+          <button className="highlight" value="*" onClick={handleClick}>&times;</button>
+          <button value="4" onClick={handleClick}>4</button>
+          <button value="5" onClick={handleClick}>5</button>
+          <button value="6" onClick={handleClick}>6</button>
+          <button className="highlight" value="-" onClick={handleClick}>&ndash;</button>
+          <button value="1" onClick={handleClick}>1</button>
+          <button value="2" onClick={handleClick}>2</button>
+          <button value="3" onClick={handleClick}>3</button>
+          <button className="highlight" value="+" onClick={handleClick}>+</button>
+          <button value="0" onClick={handleClick}>0</button>
+          <button value="*" onClick={handleClick}>.</button>
           <button className="highlight" onClick={calculate} id="result">=</button>
         </div>
       </div>
