fe80380f2800a629a645cb5447e879876662ffb3
bhartik021 <bhartik021@gmail.com>
2022-07-02 13:54:53 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -6,23 +6,23 @@ const App = () => {
 
   const handleClick = (e) => {
     setResult(result.concat(e.target.value));
-  }
+  };
 
   const clear = () => {
     setResult("");
-  }
+  };
 
   const backspace = () => {
     setResult(result.slice(0, result.length - 1));
-  }
+  };
 
   const calculate = () => {
     try {
       setResult(eval(result).toString());
-    } catch(err) {
-      setResult("Error")
+    } catch (err) {
+      setResult("Error");
     }
-  }
+  };
 
   return (
     <>
@@ -32,28 +32,64 @@ const App = () => {
         </form>
 
         <div className="keypad">
-          <button className="highlight" onClick={clear} id="clear">Clear</button>
-          <button className="highlight" onClick={backspace} id="backspace">C</button>
-          <button className="highlight" value="/" onClick={handleClick}>&divide;</button>
-          <button value="7" onClick={handleClick}>7</button>
-          <button value="8" onClick={handleClick}>8</button>
-          <button value="9" onClick={handleClick}>9</button>
-          <button className="highlight" value="*" onClick={handleClick}>&times;</button>
-          <button value="4" onClick={handleClick}>4</button>
-          <button value="5" onClick={handleClick}>5</button>
-          <button value="6" onClick={handleClick}>6</button>
-          <button className="highlight" value="-" onClick={handleClick}>&ndash;</button>
-          <button value="1" onClick={handleClick}>1</button>
-          <button value="2" onClick={handleClick}>2</button>
-          <button value="3" onClick={handleClick}>3</button>
-          <button className="highlight" value="+" onClick={handleClick}>+</button>
-          <button value="0" onClick={handleClick}>0</button>
-          <button value="*" onClick={handleClick}>.</button>
-          <button className="highlight" onClick={calculate} id="result">=</button>
+          <button className="highlight" onClick={clear} id="clear">
+            Clear
+          </button>
+          <button className="highlight" onClick={backspace} id="backspace">
+            C
+          </button>
+          <button className="highlight" value="/" onClick={handleClick}>
+            &divide;
+          </button>
+          <button value="7" onClick={handleClick}>
+            7
+          </button>
+          <button value="8" onClick={handleClick}>
+            8
+          </button>
+          <button value="9" onClick={handleClick}>
+            9
+          </button>
+          <button className="highlight" value="*" onClick={handleClick}>
+            &times;
+          </button>
+          <button value="4" onClick={handleClick}>
+            4
+          </button>
+          <button value="5" onClick={handleClick}>
+            5
+          </button>
+          <button value="6" onClick={handleClick}>
+            6
+          </button>
+          <button className="highlight" value="-" onClick={handleClick}>
+            &ndash;
+          </button>
+          <button value="1" onClick={handleClick}>
+            1
+          </button>
+          <button value="2" onClick={handleClick}>
+            2
+          </button>
+          <button value="3" onClick={handleClick}>
+            3
+          </button>
+          <button className="highlight" value="+" onClick={handleClick}>
+            +
+          </button>
+          <button value="0" onClick={handleClick}>
+            0
+          </button>
+          <button value="*" onClick={handleClick}>
+            .
+          </button>
+          <button className="highlight" onClick={calculate} id="result">
+            =
+          </button>
         </div>
       </div>
     </>
   );
-}
+};
 
 export default App;
